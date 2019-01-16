1324963fc36bda5a0bf55e2d4ba6ce1bbd06ad22
uanoob <usnet@ukr.net>
2019-01-16 15:29:22 +0200

    Create dummy todoData

--- src/app.js
+++ src/app.js
@@ -3,12 +3,31 @@ import AppHeader from './app-header';
 import SearchPanel from './search-panel';
 import TodoList from './todo-list';
 
-const App = () => (
-  <div>
-    <AppHeader />
-    <SearchPanel />
-    <TodoList />
-  </div>
-);
+const App = () => {
+  const todoData = [
+    {
+      id: 1,
+      label: 'Drink coffee',
+      important: false,
+    },
+    {
+      id: 2,
+      label: 'Drink tea',
+      important: true,
+    },
+    {
+      id: 3,
+      label: 'Drink tomato',
+      important: false,
+    },
+  ];
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
 
 export default App;
