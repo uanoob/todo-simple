7490dacc14a4681ac47ac15eec2d951b9a299c0c
uanoob <usnet@ukr.net>
2019-01-16 15:34:03 +0200

    Receive todos data & check propTypes

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => (
+const TodoList = ({ todos }) => (
   <ul>
-    <TodoListItem />
+    {todos.map((item) => {
+      const { id, ...itemProps } = item;
+      return (
+        <li key={id}>
+          <TodoListItem {...itemProps} />
+        </li>
+      );
+    })}
   </ul>
 );
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      important: PropTypes.bool.isRequired,
+      label: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default TodoList;
