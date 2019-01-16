ef42b5a3fad8bd78918e269d07931836c583b90d
uanoob <usnet@ukr.net>
2019-01-16 15:35:43 +0200

    Receive todo item data & check propTypes

--- src/todo-list-item.js
+++ src/todo-list-item.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TodoListItem = () => <li>Learn React</li>;
+const TodoListItem = ({ important, label }) => {
+  const style = {
+    color: important ? 'tomato' : 'black',
+  };
+  return <span style={style}>{label}</span>;
+};
+
+TodoListItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  important: PropTypes.bool.isRequired,
+};
 
 export default TodoListItem;
