162136660f96416f0dbe1d297410f64ee947c1f6
uanoob <usnet@ukr.net>
2019-01-17 12:18:27 +0200

    Add mark down & mark important features to todo task

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './todo-list-item.css';
 
-const TodoListItem = ({ important = false, label }) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
+class TodoListItem extends Component {
+  state = {
+    done: false,
+    important: false,
   };
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
+  onMarkDone = () => {
+    this.setState(state => ({
+      done: !state.done,
+    }));
+  };
+
+  onMarkImportant = () => {
+    this.setState(state => ({
+      important: !state.important,
+    }));
+  };
+
+  render() {
+    const { done, important } = this.state;
+    const { label } = this.props;
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
+
+    return (
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          role="button"
+          tabIndex="0"
+          onClick={this.onMarkDone}
+          onKeyDown={this.onMarkDone}
+        >
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button" className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button" className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button" className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+    );
+  }
+}
 
 TodoListItem.propTypes = {
   label: PropTypes.string.isRequired,
-  important: PropTypes.bool.isRequired,
 };
 
 export default TodoListItem;
