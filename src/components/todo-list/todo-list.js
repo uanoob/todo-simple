56579a08dc88ba2abf1cacc0feb9d6bc4bd66b70
uanoob <usnet@ukr.net>
2019-01-17 09:27:26 +0200

    Move components to own folders

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TodoListItem from './todo-list-item';
+import TodoListItem from '../todo-list-item/todo-list-item';
 import './todo-list.css';
 
 const TodoList = ({ todos }) => (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import reactDOM from 'react-dom';
-import App from './components/app';
+import App from './components/app/app';
 
 reactDOM.render(<App />, document.getElementById('root'));
