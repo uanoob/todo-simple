abf373bed26a28e61d805fd69113837dd492d9f8
uanoob <usnet@ukr.net>
2019-01-17 09:14:32 +0200

    Add to TodoListItem -> buttons: trash & exclamation

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './todo-list-item.css';
 
-const TodoListItem = ({ important, label }) => {
+const TodoListItem = ({ important = false, label }) => {
   const style = {
-    color: important ? 'tomato' : 'black',
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal',
   };
-  return <span style={style}>{label}</span>;
+
+  return (
+    <span className="todo-list-item">
+      <span className="todo-list-item-label" style={style}>
+        {label}
+      </span>
+
+      <button type="button" className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button" className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
 };
 
 TodoListItem.propTypes = {
