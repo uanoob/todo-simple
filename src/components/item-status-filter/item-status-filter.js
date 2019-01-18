082ffbb91cc6537072c3dbea4cb1a6a744ffe33a
uanoob <usnet@ukr.net>
2019-01-18 15:40:08 +0200

    Change ItemStatusFilter to stateless function

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,41 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './item-status-filter.css';
 
-class ItemStatusFilter extends Component {
-  state = {};
-
-  render() {
-    const { onStatusFilter, active } = this.props;
-    return (
-      <div className="btn-group">
-        <button
-          type="button"
-          className={`btn btn-outline-info ${active === 'all' ? 'active' : ''}`}
-          aria-pressed="true"
-          onClick={() => onStatusFilter('all')}
-        >
-          All
-        </button>
-        <button
-          type="button"
-          className={`btn btn-outline-info ${active === 'active' ? 'active' : ''}`}
-          onClick={() => onStatusFilter('active')}
-        >
-          Active
-        </button>
-        <button
-          type="button"
-          className={`btn btn-outline-info ${active === 'done' ? 'active' : ''}`}
-          onClick={() => onStatusFilter('done')}
-        >
-          Done
-        </button>
-      </div>
-    );
-  }
-}
+const ItemStatusFilter = ({ onStatusFilter, active }) => (
+  <div className="btn-group">
+    <button
+      type="button"
+      className={`btn btn-outline-info ${active === 'all' ? 'active' : ''}`}
+      aria-pressed="true"
+      onClick={() => onStatusFilter('all')}
+    >
+      All
+    </button>
+    <button
+      type="button"
+      className={`btn btn-outline-info ${active === 'active' ? 'active' : ''}`}
+      onClick={() => onStatusFilter('active')}
+    >
+      Active
+    </button>
+    <button
+      type="button"
+      className={`btn btn-outline-info ${active === 'done' ? 'active' : ''}`}
+      onClick={() => onStatusFilter('done')}
+    >
+      Done
+    </button>
+  </div>
+);
 
 ItemStatusFilter.propTypes = {
   onStatusFilter: PropTypes.func.isRequired,
